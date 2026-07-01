93a992218555a710e14f457709c563b024839f75
kalisilent <ronak.tiwari@gmail.com>
2026-07-01 12:34:18 +0530

    Remove auth, remove interview history

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,20 +2,8 @@ import React from 'react';
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
-import InterviewCard from "@/components/InterviewCard";
-import { getCurrentUser } from "@/lib/actions/auth.action";
-import { getInterviewsByUserId } from "@/lib/actions/general.action";
-import { redirect } from 'next/navigation';
 
 const page = async () => {
-  const user = await getCurrentUser();
-
-  if (!user) redirect('/sign-in');
-
-  const userInterviews = await getInterviewsByUserId(user?.id!);
-
-  const hasPastInterviews = (userInterviews?.length ?? 0) > 0;
-
   return (
     <>
       <section className="card-cta">
@@ -24,12 +12,10 @@ const page = async () => {
           <p className="text-lg">
             Practice real interview questions & get instant feedback
           </p>
-
           <Button asChild className="btn-primary max-sm:w-full">
             <Link href="/interview">Start an Interview</Link>
           </Button>
         </div>
-
         <Image
           src="/robot2.png"
           alt="robo-dude"
@@ -37,19 +23,6 @@ const page = async () => {
           height={400}
           className="max-sm:hidden"
         />
-      </section>
-
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Your Interviews</h2>
-        <div className="interviews-section">
-          {hasPastInterviews ? (
-            userInterviews?.map((interview) => (
-              <InterviewCard {...interview} key={interview.id} />
-            ))
-          ) : (
-            <p>You haven&apos;t taken any interviews yet</p>
-          )}
-        </div>
       </section>
     </>
   );
